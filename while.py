10087f29c0f6c0ab0ebd2baa03c732ead8df69bf
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2024-04-17 21:19:10 +0530

    Create while.py
    simple while example code

---
+++ while.py
@@ -0,0 +1,26 @@
+i=55
+while(i<97):
+    i=i+1
+    print(i)
+
+i=1223
+while(i>995):
+    i=i-1
+    print(i)
+
+i=0
+while(i<50):
+    i=i+5
+    print(i)
+
+i=77
+while(i>7):
+    i=i-7
+    print(i)
+    
+    i=10
+while(i<56):
+    i=i+2
+    print(i)
+
+
