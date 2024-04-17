31b58925904b25713c2860488655bbbe5b2cbc6b
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2024-04-17 21:14:55 +0530

    Create dictionary.py
    example code to understand dictionary

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+word = input()
+print(word)
+
+
+store = dict()
+print(store)
+store['p'] = 23 
+store['Q'] = "abcd"
+store['w'] = 22
+store[12] = 11
+store["hello"] = 222
+print(store)
+
+allKeys = store.keys()
+print(allKeys)
