5c13f775115574aa1f40e67c11b59cf4f3bba1a6
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2024-04-17 21:20:23 +0530

    Create class.py
    example code for classes

---
+++ class.py
@@ -0,0 +1,51 @@
+class block:
+    def __init__(self,cname,crollno,cdbmsmarks,cosmarks,crmarks,cjavamarks,cdatamarks):
+        self.name=cname
+        self.rollno=crollno
+        self.dbmsmarks=cdbmsmarks
+        self.osmarks=cosmarks
+        self.rmarks=crmarks
+        self.javamarks=cjavamarks
+        self.datamarks=cdatamarks
+
+student1=block("shiva",78,82,45,63,46,89)
+print(student1.name)
+print(student1.rollno)
+print(student1.dbmsmarks)
+print(student1.osmarks)
+print(student1.rmarks)
+print(student1.javamarks)
+print(student1.datamarks)
+
+student2=block("poojitha balu",7,8,4,6,4,8)
+print(student2.name)
+print(student2.rollno)
+print(student2.dbmsmarks)
+print(student2.osmarks)
+print(student2.rmarks)
+print(student2.javamarks)
+print(student2.datamarks)
+
+student3=block("muthu balu",71,8,43,6,45,8)
+print(student3.name)
+print(student3.rollno)
+print(student3.dbmsmarks)
+print(student3.osmarks)
+print(student3.rmarks)
+print(student3.javamarks)
+print(student3.datamarks)
+
+student4=block("varshini balu",7,8,43,6,4,8)
+print(student4.name)
+print(student4.rollno)
+print(student4.dbmsmarks)
+print(student4.osmarks)
+print(student4.rmarks)
+print(student4.javamarks)
+print(student4.datamarks)
+
+
+
+
+
+
