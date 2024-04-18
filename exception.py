f32d47b79d9f5fce631e4c7e32440df8b2cdea3a
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2024-04-18 20:49:09 +0530

    example code how exceptions handled

---
+++ exception.py
@@ -0,0 +1,42 @@
+print("exception handling in python")
+try:
+    a=4/0
+except:
+    print("exception occured")
+else:
+    print("no exception occured")
+finally:
+    print("this is finally block ")
+print("end!!")
+
+
+print("exception handling in python")
+a=[10,30,40,60]
+try:
+    print(a[0])
+except:
+    print("exception occured")
+else:
+    print("no exception occured")
+finally:
+    print("this is finally block ")
+print("end!!")
+
+
+print("exception handling in python")
+j=80
+try:
+    print(f)
+except:
+    print("exception occured")
+else:
+    print("no exception occured")
+try:
+    print(k)
+except:
+    print("exception")
+else:
+    print("not occured")
+finally:
+    print("this is finally block ")
+print("end!!")
