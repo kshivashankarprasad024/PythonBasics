65199e5db10075b85abf0afd78539a2c505318fe
kshivashankarprasad024 <167082246+kshivashankarprasad024@users.noreply.github.com>
2024-04-18 20:50:43 +0530

    exception example codes

---
+++ exception1.py
@@ -0,0 +1,54 @@
+print("exception handling in python")
+a=[20,30,50,100]
+try:
+    print(5/0)
+except IndexError:
+    print("exception occured")
+except ZeroDivisionError:
+    print("zero division error")
+except:
+    print("exception")
+else:
+    print("no exception occured")
+finally:
+    print("this is finally block ")
+print("end!!")
+
+
+
+print("exception handling in python")
+a=[20,30,50,100]
+try:
+    print(a[7])
+except IndexError:
+    print("exception occured")
+except ZeroDivisionError:
+    print("zero division error")
+except:
+    print("exception")
+else:
+    print("no exception occured")
+finally:
+    print("this is finally block ")
+print("end!!")
+
+
+print("exception handling in python")
+a=[20,30,50,100]
+j=20
+try:
+    print(y)
+except IndexError:
+    print("exception occured")
+except ZeroDivisionError:
+    print("zero division error")
+except NameError:
+    print("name error")
+except:
+    print("exception")
+else:
+    print("no exception occured")
+finally:
+    print("this is finally block ")
+print("end!!")
+
